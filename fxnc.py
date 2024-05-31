36898b835d6a909daa802bdae3d1a6fa5eb81b24
Yusuf Olokoba <olokobayusuf@gmail.com>
2024-05-31 16:24:50 -0400

    Minor mod

--- fxnc.py
+++ fxnc.py
@@ -36,35 +36,35 @@ def main (): # CHECK # Linux
     LIB_PATH_BASE = Path("Packages") / "ai.fxn.fxn3d" / "Plugins"
     LIBS = [
         {
-            "url": f"https://cdn.fxn.ai/edgefxn/{version}/libFunction-android-arm64-v8a.so",
+            "url": f"https://cdn.fxn.ai/fxnc/{version}/libFunction-android-arm64-v8a.so",
             "path": LIB_PATH_BASE / "Android" / "arm64-v8a" / "libFunction.so"
         },
         {
-            "url": f"https://cdn.fxn.ai/edgefxn/{version}/libFunction-android-armeabi-v7a.so",
+            "url": f"https://cdn.fxn.ai/fxnc/{version}/libFunction-android-armeabi-v7a.so",
             "path": LIB_PATH_BASE / "Android" / "armeabi-v7a" / "libFunction.so"
         },
         {
-            "url": f"https://cdn.fxn.ai/edgefxn/{version}/libFunction-android-x86.so",
+            "url": f"https://cdn.fxn.ai/fxnc/{version}/libFunction-android-x86.so",
             "path": LIB_PATH_BASE / "Android" / "x86" / "libFunction.so"
         },
         {
-            "url": f"https://cdn.fxn.ai/edgefxn/{version}/libFunction-android-x86_64.so",
+            "url": f"https://cdn.fxn.ai/fxnc/{version}/libFunction-android-x86_64.so",
             "path": LIB_PATH_BASE / "Android" / "x86_64" / "libFunction.so"
         },
         {
-            "url": f"https://cdn.fxn.ai/edgefxn/{version}/Function-ios-iphoneos.framework.zip",
+            "url": f"https://cdn.fxn.ai/fxnc/{version}/Function-ios-iphoneos.framework.zip",
             "path": LIB_PATH_BASE / "iOS" / "Function.framework.zip"
         },
         {
-            "url": f"https://cdn.fxn.ai/edgefxn/{version}/Function-macos.dylib",
+            "url": f"https://cdn.fxn.ai/fxnc/{version}/Function-macos.dylib",
             "path": LIB_PATH_BASE / "macOS" / "Function.dylib"
         },
         {
-            "url": f"https://cdn.fxn.ai/edgefxn/{version}/Function.wasm",
+            "url": f"https://cdn.fxn.ai/fxnc/{version}/Function.wasm",
             "path": LIB_PATH_BASE / "Web" / "libFunction.so"
         },
         {
-            "url": f"https://cdn.fxn.ai/edgefxn/{version}/Function-win64.dll",
+            "url": f"https://cdn.fxn.ai/fxnc/{version}/Function-win64.dll",
             "path": LIB_PATH_BASE / "Windows" / "Function.dll"
         },
     ]
